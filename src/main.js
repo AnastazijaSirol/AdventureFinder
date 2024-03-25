4931cb069d4b800205b6e7598d64db477bbf9e9a
AnastazijaSirol <119959886+AnastazijaSirol@users.noreply.github.com>
2024-03-25 19:02:50 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {createRouter, createWebHistory} from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue';
+import prijava_stranica from './components/prijava_stranica.vue';
 
-createApp(App).mount('#app')
+const routes=[
+    {path: '/', component: HelloWorld},
+    {path: '/prijava_stranica', component: () => import ('./components/prijava_stranica.vue')},
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+const app = createApp(App);
+app.component('prijava_stranica', prijava_stranica);
+app.component('HelloWorld', HelloWorld);
+app.use(router);
+app.mount('#app');
