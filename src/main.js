3a2078831115c809d91e66c6cdbb5a066cefa0ec
AnastazijaSirol <119959886+AnastazijaSirol@users.noreply.github.com>
2024-03-26 10:09:28 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import {createRouter, createWebHistory} from 'vue-router';
 import HelloWorld from './components/HelloWorld.vue';
 import prijava_stranica from './components/prijava_stranica.vue';
+import registracija_stranica from './components/registracija_stranica.vue';
 
 const routes=[
     {path: '/', component: HelloWorld},
     {path: '/prijava_stranica', component: () => import ('./components/prijava_stranica.vue')},
+    {path: '/registracija_stranica', component: () => import ('./components/registracija_stranica.vue')},
 ];
 
 const router = createRouter({
@@ -16,6 +18,8 @@ const router = createRouter({
 
 const app = createApp(App);
 app.component('prijava_stranica', prijava_stranica);
-app.component('HelloWorld', HelloWorld);
+app.component('/', HelloWorld);
+app.component('registracija_stranica', registracija_stranica);
+
 app.use(router);
 app.mount('#app');
