f5c90788711e90f5c32bc070ce87d3fce3a42191
AnastazijaSirol <119959886+AnastazijaSirol@users.noreply.github.com>
2024-03-26 07:36:54 +0100

    Create prijava_stranica.vue

---
+++ src/components/prijava_stranica.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id="app">
+    <button class="pocetna-button" @click="usmjeri_pocetna">Početna</button>
+    <h1 class="naslov">Prijava</h1>
+    <div class="kvadrat">
+      <form class="forma">
+        <input type="text" placeholder="Korisničko ime" />
+        <input type="password" placeholder="Lozinka" />
+        <button @click="usmjeri_aktivnosti">Prijava</button>
+      </form>
+    </div>
+    <p class="poruka">Već imate korisnički račun? <b class="preporuka" @click="usmjeri_registracija">Registrirajte se.</b></p>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods: {
+    usmjeri_pocetna() {
+      this.$router.push('/');
+    },
+    usmjeri_registracija() {
+      this.$router.push('/registracija');
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+/* Stilovi */
+html, body {
+  background-color: #1B1C1B;
+}
+
+.pocetna-button {
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  padding: 10px 20px;
+  background-color: #D9D9D9;
+  color: #1B1C1B;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.kvadrat {
+  background-color: #D9D9D9;
+  padding: 20px;
+  border-radius: 10px;
+  margin: 0 auto;
+  max-width: 400px;
+}
+
+.forma input[type="text"],
+.forma input[type="password"],
+.forma button {
+  margin-bottom: 10px;
+  width: calc(100% - 20px);
+  padding: 10px;
+  border-radius: 5px;
+  border: none;
+}
+
+.forma button {
+  background-color: #1B1C1B;
+  color: #D9D9D9;
+  cursor: pointer;
+}
+
+.forma button:hover {
+  background-color: #1d2935;
+}
+
+.poruka {
+  color: #D9D9D9;
+  margin-top: 10px;
+}
+
+.preporuka {
+  color: #D9D9D9;
+  cursor: pointer;
+}
+
+</style>
