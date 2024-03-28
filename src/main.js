008dd19197e9af61e6f948cba48ca328da41403d
AnastazijaSirol <119959886+AnastazijaSirol@users.noreply.github.com>
2024-03-28 16:28:35 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import {createRouter, createWebHistory} from 'vue-router';
-import HelloWorld from './components/HelloWorld.vue';
+import pocetna_stranica from './components/pocetna_stranica.vue';
 import prijava_stranica from './components/prijava_stranica.vue';
 import registracija_stranica from './components/registracija_stranica.vue';
+import prijava_administrator from './components/prijava_administrator.vue';
 import aktivnosti_stranica from './components/aktivnosti_stranica.vue';
 
 const routes=[
-    {path: '/', component: HelloWorld},
+    {path: '/', component: pocetna_stranica},
     {path: '/prijava_stranica', component: () => import ('./components/prijava_stranica.vue')},
     {path: '/registracija_stranica', component: () => import ('./components/registracija_stranica.vue')},
+    {path: '/prijava_administrator', component: () => import ('./components/prijava_administrator.vue')},
     {path: '/aktivnosti_stranica', component: () => import ('./components/aktivnosti_stranica.vue')},
 ];
 
@@ -19,9 +21,10 @@ const router = createRouter({
 });
 
 const app = createApp(App);
+app.component('/', pocetna_stranica);
 app.component('prijava_stranica', prijava_stranica);
-app.component('/', HelloWorld);
 app.component('registracija_stranica', registracija_stranica);
+app.component('prijava_administrator', prijava_administrator);
 app.component('aktivnosti_stranica', aktivnosti_stranica);
 
 app.use(router);
