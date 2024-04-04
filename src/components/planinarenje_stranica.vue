dba23965abbb13b32218bc6f7da6422303a08b11
AnastazijaSirol <119959886+AnastazijaSirol@users.noreply.github.com>
2024-04-04 08:14:45 +0200

    Update planinarenje_stranica.vue

--- src/components/planinarenje_stranica.vue
+++ src/components/planinarenje_stranica.vue
@@ -1,12 +1,19 @@
 <template>
-
-<div id="app">
-    <button class="natrag" @click="usmjeri_aktivnosti">Natrag</button>
-    <button class="odjava" @click="usmjeri_pocetna">Odjava</button>
-    <h2 class="naziv_aktivnosti">Planinarenje</h2>
-    <div><button class="dodaj_destinaciju" @click="dodaj_des">Dodaj destinaciju</button></div>
-</div>
-
+    <div id="app">
+        <button class="natrag" @click="usmjeri_aktivnosti">Natrag</button>
+        <button class="odjava" @click="usmjeri_pocetna">Odjava</button>
+        <h2 class="naziv_aktivnosti">Planinarenje</h2>
+        <div class="dodaj_i_filter">
+            <button class="dodaj_destinaciju" @click="dodaj_des">Dodaj destinaciju</button>
+            <div class="filter_destinacije">
+                <div class="filter_tekst">Filter destinacija</div>
+                <select v-model="sortiranje" class="filter">
+                    <option value="asc">Poredaj po ocijenjenosti uzlazno</option>
+                    <option value="desc">Poredaj po ocijenjenosti silazno</option>
+                </select>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -57,14 +64,37 @@ html, body {
 }
 
 .dodaj_destinaciju {
-  position: absolute;
-  left: 20px;
   background: none;
   border: none;
   color: #D9D9D9;
   cursor: pointer;
   margin-left: 10px;
   font-size: 1.3em;
+}
+
+.dodaj_i_filter {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.filter_destinacije {
+  margin-top: 30px;
+}
+
+.filter_tekst {
+  color: #D9D9D9;
+  font-size: 1.2em;
+}
+
+.filter {
+  border: none;
+  background-color: #333;
+  color: #D9D9D9;
+  padding: 5px;
+  cursor: pointer;
+  font-size: 1em;
+  border-radius: 5px;
 }
 
 </style>
