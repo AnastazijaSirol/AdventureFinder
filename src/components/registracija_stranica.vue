6a58052e7e2d741f1e8c935954291323953b98ba
AnastazijaSirol <119959886+AnastazijaSirol@users.noreply.github.com>
2024-04-09 08:41:50 +0200

    Update registracija_stranica.vue

--- src/components/registracija_stranica.vue
+++ src/components/registracija_stranica.vue
@@ -1,40 +1,39 @@
 <template>
-  <div id="app">
-    <button class="pocetna-button" @click="usmjeri_pocetna">Početna</button>
-    <h1 class="naslov">Registracija</h1>
-    <div class="kvadrat">
-      <form class="forma">
-        <input type="text" placeholder="Korisničko ime" v-model="korisnickoIme" />
-        <input type="email" placeholder="E-pošta" v-model="email" />
-        <input type="password" placeholder="Lozinka" v-model="lozinka" />
-        <input type="password" placeholder="Potvrdi lozinku" v-model="potvrdaLozinke" />
-        <button @click="usmjeri_aktivnosti">Registracija</button>
-      </form>
+    <div id="app">
+        <button class="pocetna-button" @click="usmjeri_pocetna">Početna</button>
+        <h1 class="naslov">Registracija</h1>
+        <div class="kvadrat">
+            <form class="forma">
+                <input type="text" placeholder="Korisničko ime" v-model="korisnickoIme" />
+                <input type="email" placeholder="E-pošta" v-model="email" />
+                <input type="password" placeholder="Lozinka" v-model="lozinka" />
+                <input type="password" placeholder="Potvrdi lozinku" v-model="potvrdaLozinke" />
+                <button @click="usmjeri_aktivnosti">Registracija</button>
+            </form>
+        </div>
+        <p class="poruka">Već imate korisnički račun? <b class="preporuka" @click="usmjeri_prijava">Prijavi se.</b></p>
+        <router-view/>
     </div>
-    <p class="poruka">Već imate korisnički račun? <b class="preporuka" @click="usmjeri_prijava">Prijavi se.</b></p>
-    <router-view/>
-  </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  methods: {
+name: 'App',
+methods: {
     usmjeri_pocetna() {
-      this.$router.push('/');
+        this.$router.push('/');
     },
     usmjeri_aktivnosti() {
         this.$router.push('aktivnosti_stranica');
     },
     usmjeri_prijava() {
-      this.$router.push('prijava_stranica');
+        this.$router.push('prijava_stranica');
     }
   }
 };
 </script>
 
 <style lang="scss">
-
 html, body {
   background-color: #1B1C1B;
 }
@@ -89,5 +88,4 @@ html, body {
   color: #D9D9D9;
   cursor: pointer;
 }
-
 </style>
