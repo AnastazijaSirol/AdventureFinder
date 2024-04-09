bf6c3b467a5f5c581fa9e0b4ca75d7d7b3ee512e
AnastazijaSirol <119959886+AnastazijaSirol@users.noreply.github.com>
2024-04-09 08:37:19 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,17 +4,19 @@ import {createRouter, createWebHistory} from 'vue-router';
 import pocetna_stranica from './components/pocetna_stranica.vue';
 import prijava_stranica from './components/prijava_stranica.vue';
 import registracija_stranica from './components/registracija_stranica.vue';
-import prijava_administrator from './components/prijava_administrator.vue';
 import aktivnosti_stranica from './components/aktivnosti_stranica.vue';
 import planinarenje_stranica from './components/planinarenje_stranica.vue';
+import padobran_stranica from './components/padobran_stranica.vue';
+import dodavanje_destinacije from './components/dodavanje_destinacije.vue';
 
 const routes=[
     {path: '/', component: pocetna_stranica},
     {path: '/prijava_stranica', component: () => import ('./components/prijava_stranica.vue')},
     {path: '/registracija_stranica', component: () => import ('./components/registracija_stranica.vue')},
-    {path: '/prijava_administrator', component: () => import ('./components/prijava_administrator.vue')},
     {path: '/aktivnosti_stranica', component: () => import ('./components/aktivnosti_stranica.vue')},
     {path: '/planinarenje_stranica', component: () => import ('./components/planinarenje_stranica.vue')},
+    {path: '/padobran_stranica', component: () => import ('./components/padobran_stranica.vue')},
+    {path: '/dodavanje_destinacije', component: () => import('./components/dodavanje_destinacije.vue')},
 ];
 
 const router = createRouter({
@@ -23,12 +25,14 @@ const router = createRouter({
 });
 
 const app = createApp(App);
-app.component('/', pocetna_stranica);
+app.component('pocetna_stranica', pocetna_stranica);
 app.component('prijava_stranica', prijava_stranica);
 app.component('registracija_stranica', registracija_stranica);
-app.component('prijava_administrator', prijava_administrator);
 app.component('aktivnosti_stranica', aktivnosti_stranica);
 app.component('planinarenje_stranica', planinarenje_stranica);
+app.component('padobran_stranica', padobran_stranica);
+app.component('dodavanje_destinacije', dodavanje_destinacije);
 
 app.use(router);
 app.mount('#app');
+
