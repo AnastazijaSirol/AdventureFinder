627c50a2b4e0141d484d6cae552cbf9b60bb48df
AnastazijaSirol <119959886+AnastazijaSirol@users.noreply.github.com>
2024-04-22 21:29:21 +0200

    Create dodavanje_destinacije_prezivljavanje.vue

---
+++ src/components/dodavanje_destinacije_prezivljavanje.vue
@@ -0,0 +1,81 @@
+<template>
+    <div id="app">
+        <button class="odustani" @click="usmjeri_natrag">Odustani</button>
+        <h2 class="naslov">Dodavanje destinacije</h2>
+        <div class="kvadrat">
+            <form class="forma">
+                <input type="text" placeholder="Naziv destinacije" v-model="nazivdestinacije"/>
+                <input type="text" placeholder="Država" v-model="drzava"/>
+                <input type="text" placeholder="Vrijeme trajanja" v-model="vrijemetrajanja"/>
+                <input type="text" placeholder="Naplata" v-model="naplata"/>
+                <input type="text" placeholder="Potrebna oprema" v-model="potrebnaoprema"/>
+                <input type="text" placeholder="Poveznica za rezervaciju" v-model="poveznicazarezervaciju"/>
+                <button @click.prevent="potvrdi" class="potvrdi">Potvrdi</button>
+            </form>
+        </div>
+        <router-view/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    methods: {
+        usmjeri_natrag() {
+            this.$router.push('prezivljavanje_stranica');
+        },
+        potvrdi() {
+            this.$router.push('prezivljavanje_stranica');
+        }
+    }
+};
+</script>
+
+<style lang="scss">
+html, body {
+    background-color: #1B1C1B;
+    color: #D9D9D9;
+}
+
+.odustani {
+    position: absolute;
+    top: 20px;
+    left: 20px;
+    background: none;
+    border: none;
+    color: #D9D9D9;
+    cursor: pointer;
+    margin-left: 10px;
+    font-size: 1em;
+}
+
+.naslov {
+    font-size: 24px;
+    margin-bottom: 20px;
+}
+
+.kvadrat {
+    background-color: #D9D9D9;
+    padding: 20px;
+    border-radius: 10px;
+}
+
+.forma input[type="text"],
+.forma button {
+    margin-bottom: 10px;
+    width: calc(100% - 20px);
+    padding: 10px;
+    border-radius: 5px;
+    border: none;
+}
+
+.forma button {
+    background-color: #1B1C1B;
+    color: #D9D9D9;
+    cursor: pointer;
+}
+
+.forma button:hover {
+    background-color: #1d2935;
+}
+</style>
