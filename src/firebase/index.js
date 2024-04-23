0be7cc28212c88a3c1b671000a34b2d37ffdc7e8
AnastazijaSirol <119959886+AnastazijaSirol@users.noreply.github.com>
2024-04-23 16:39:15 +0200

    Add files via upload

---
+++ src/firebase/index.js
@@ -0,0 +1,25 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCd0MwbRwaGc1MobDYvbRBNxmYI8LGExKs",
+  authDomain: "adventure-finder-d509e.firebaseapp.com",
+  projectId: "adventure-finder-d509e",
+  storageBucket: "adventure-finder-d509e.appspot.com",
+  messagingSenderId: "844998571550",
+  appId: "1:844998571550:web:62c9d7ef039ddb3224e00f"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db=getFirestore(app);
+const auth=getAuth(app);
+const storage=getStorage(app);
+
+export {
+    db,
+    auth,
+    storage
+}
