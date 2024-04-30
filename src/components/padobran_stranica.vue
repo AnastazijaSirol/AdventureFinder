df67bf88a8ff0727664c97055f4d6deca71dcccc
AnastazijaSirol <119959886+AnastazijaSirol@users.noreply.github.com>
2024-04-30 10:06:25 +0200

    Update padobran_stranica.vue

--- src/components/padobran_stranica.vue
+++ src/components/padobran_stranica.vue
@@ -57,7 +57,7 @@ export default {
             this.$router.push('dodavanje_destinacije_padobran');
         },
         prikaziDetalje(id) {
-            console.log('Prikaz detalja destinacije s ID-om:', id);
+            this.$router.push({ name: 'padobran_destinacija', params: { destinacijaId: id } });
         }
     },
     mounted() {
