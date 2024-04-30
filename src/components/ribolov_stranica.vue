e7fb9e99009a6189eae869f0070f84e6f34d6a27
AnastazijaSirol <119959886+AnastazijaSirol@users.noreply.github.com>
2024-04-30 10:28:01 +0200

    Update ribolov_stranica.vue

--- src/components/ribolov_stranica.vue
+++ src/components/ribolov_stranica.vue
@@ -57,7 +57,7 @@ export default {
             this.$router.push('dodavanje_destinacije_ribolov');
         },
         prikaziDetalje(id) {
-            console.log('Prikaz detalja destinacije s ID-om:', id);
+            this.$router.push({ name: 'ribolov_destinacija', params: { destinacijaId: id } });
         }
     },
     mounted() {
