a5d7c5641b32c94eadf2e0dd078ba3d602c71f12
AnastazijaSirol <119959886+AnastazijaSirol@users.noreply.github.com>
2024-05-08 21:51:16 +0200

    Update korisnicki_racun.vue

--- src/components/korisnicki_racun.vue
+++ src/components/korisnicki_racun.vue
@@ -1,85 +1,115 @@
 <template>
-  <div id="app">
+    <div id="app">
       <div class="navigacija">
-          <button class="natrag" @click="natrag">Natrag</button>
-          <button class="odjava" @click="odjava">Odjava</button>
+        <button class="natrag" @click="natrag">Natrag</button>
+        <button class="odjava" @click="odjava">Odjava</button>
       </div>
       <div class="naslov"><b>Korisnički račun</b></div>
       <div class="informacije">
-          <div v-if="currentUser">E-pošta: {{ currentUser.email }}</div>
-          <div>Posjećeno: </div>
+        <div v-if="currentUser">E-pošta: {{ currentUser.email }}</div>
+        <div>
+          <span> Ažuriranje financija: </span> 
+          <input type="number" v-model="financije" @change="azurirajFinancije" />
+          €
+        </div>
+        <div>Posjećeno:</div>
       </div>
-  </div>
-</template>
+    </div>
+  </template>
 
-<script>
-import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from '@/firebase';
+  <script>
+  import { onAuthStateChanged } from 'firebase/auth';
+  import { doc, getDoc, getDocs, query, collection, setDoc } from 'firebase/firestore';
+  import { db, auth } from '@/firebase';
 
-export default {
-  data() {
+  export default {
+    data() {
       return {
-          currentUser: null
+        currentUser: null,
+        financije: localStorage.getItem('financije') || 0
       };
-  },
-  methods: {
+    },
+    methods: {
       natrag() {
-          this.$router.push('aktivnosti_stranica');
+        this.$router.push({ name: 'aktivnosti_stranica' });
       },
       odjava() {
-          this.$router.push('pocetna_stranica');
+        localStorage.removeItem('financije');
+        this.$router.push({ name: 'pocetna_stranica' });
+      },
+      async azurirajFinancije() {
+        if (!this.currentUser) {
+          console.error('Korisnik nije prijavljen.');
+          return;
+        }
+        try {
+          const userId = this.currentUser.uid;
+          await setDoc(doc(db, 'registrirani', userId), {
+            financije: this.financije
+          });
+          console.log('Financije su ažurirane.');
+        } catch (error) {
+          console.error('Došlo je do pogreške prilikom ažuriranja financija:', error);
+        }
       }
-  },
-  mounted() {
-      onAuthStateChanged(auth, (user) => {
-          if (user) {
-              this.currentUser = user;
-          } else {
-              this.currentUser = null;
+    },
+    mounted() {
+      onAuthStateChanged(auth, async (user) => {
+        if (user) {
+          this.currentUser = user;
+          const userId = user.uid;
+          const userDoc = await getDoc(doc(db, 'registrirani', userId));
+          if (userDoc.exists()) {
+            this.financije = userDoc.data().financije || 0;
           }
+        } else {
+          this.currentUser = null;
+        }
       });
+    }
+  };
+  </script>
+
+  <style lang="scss">
+  .odjava {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    padding: 10px 20px;
+    background-color: #D9D9D9;
+    color: #1B1C1B;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    margin-left: 10px;
   }
-};
-</script>
 
-<style lang="scss">
-.odjava {
-position: absolute;
-top: 20px;
-right: 20px;
-background: none;
-border: none;
-color: #D9D9D9;
-cursor: pointer;
-margin-left: 10px;
-font-size: 1em;
-}
+  .natrag {
+    position: absolute;
+    top: 20px;
+    left: 20px;
+    padding: 10px 20px;
+    background-color: #D9D9D9;
+    color: #1B1C1B;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    margin-left: 10px;
+  }
 
-.natrag {
-position: absolute;
-top: 20px;
-left: 20px;
-background: none;
-border: none;
-color: #D9D9D9;
-cursor: pointer;
-margin-left: 10px;
-font-size: 1em;
-}
+  .naslov {
+    color: #D9D9D9;
+    font-size: 1.5em;
+    margin-top: 20px;
+  }
 
-.naslov {
-color: #D9D9D9;
-font-size: 1.5em;
-margin-top: 20px;
-}
+  .informacije {
+    color: #D9D9D9;
+    margin-top: 20px;
+    font-size: 1.1em;
+  }
 
-.informacije {
-color: #D9D9D9;
-margin-top: 20px;
-font-size: 1.1em;
-}
-
-.informacije div {
-margin-bottom: 10px;
-}
-</style>
+  .informacije div {
+    margin-bottom: 10px;
+  }
+  </style>
