e149afc532150141bda229211a835629c7970010
AnastazijaSirol <119959886+AnastazijaSirol@users.noreply.github.com>
2024-05-09 19:07:07 +0200

    Add files via upload

---
+++ src/components/mapa_prikaz.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="mapa" style="width: 100%; height: 200px;">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d89005.01814168721!2d15.971123200000001!3d45.8031104!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1shr!2shr!4v1715273168869!5m2!1shr!2shr" width="600" height="450" style="border:0;" allowfullscreen="true" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+</template>
+<script></script>
+<style>
+</style>
